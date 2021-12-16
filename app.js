4c433060ea8537fe960fd0409e7b3d78e55df9d9
smiethereens23nas <nicholas23smith@gmail.com>
2021-12-16 17:13:15 -0500

    Commit 1 - Verbal questions

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+1. What is the difference between a **parameter** and an **argument**?
+A parameter is added to the beginning of a function as a placeholder - function name (param, param) and arguments are values you use when you invoke the function.
+
+2. Within a function, what is the difference between **return** and **console.log**?
+return saves a value that can be used later on. Console.log doesn't save anything, but prints out the result.
+
+3. What are the implications of the ability of a function to return a value?
+You can use that returned value in your other functions or later in your code.
+*/
