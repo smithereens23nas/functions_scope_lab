0bb1f6586092be9c5884d9b87acdfe89669c409e
smiethereens23nas <nicholas23smith@gmail.com>
2021-12-16 17:28:07 -0500

    Commit 3 - Is a Vowel

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ function calculateCube(num){
 }
 console.log(calculateCube(5));
 
+// ## 3. Is a Vowel? 
+// Write a function `isAVowel` that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise. The vowel could be upper or lower case.
+function isAVowel(letter){
+    if(letter.lowerCase === "a" || "e" || "i" || "o" || "u"){
+return `${letter} is a vowel`
+    } else{
+        return false;
+    }
+}
+console.log(isAVowel("a"));
