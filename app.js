bb630f5f8501f30c83917a85a45df4b43a9f6c22
smiethereens23nas <nicholas23smith@gmail.com>
2021-12-16 17:43:04 -0500

    Commit 4 - Two Lengths

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ You can use that returned value in your other functions or later in your code.
 function calculateCube(num){
     return num * num * num;
 }
-console.log(calculateCube(5));
+// console.log(calculateCube(5));
 
 // ## 3. Is a Vowel? 
 // Write a function `isAVowel` that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise. The vowel could be upper or lower case.
@@ -24,4 +24,13 @@ return `${letter} is a vowel`
         return false;
     }
 }
-console.log(isAVowel("a"));
+// console.log(isAVowel("a"));
+
+// ## 4. Get Two Lengths
+
+// Write a function `getTwoLengths` that accepts two parameters (strings). The function should return an _array_ of numbers where each number is the length of the corresponding string.
+
+function getTwoLengths(word1, word2){
+    return [word1.length, word2.length];
+}
+console.log(getTwoLengths("Hank", "Hippopopalous"));
